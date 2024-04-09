95def0e98dba23552ff3e55038bd0a6791df3fec
Mykal Machon <mykal@tinybox.dev>
2024-04-08 18:19:11 -0700

    feat(app): testing build with public folder

--- services/app/src/App.tsx
+++ services/app/src/App.tsx
@@ -1,11 +1,9 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
+import { useEffect, useState } from 'react'
 import { API_URL } from './utils/api'
 
 function App() {
-  const [count, setCount] = useState(0)
   const [data, setData] = useState<string | null>(null);
 
   useEffect(() => {
@@ -19,35 +17,16 @@ function App() {
   }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+    <main>
+      <img src='./favicon.svg' alt="coffee cup" className="logo" />
       <h1>coffee.mykal.codes</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-
-      </div>
-      <p>
+      <p className="read-the-docs">
         Responses from the API go below
       </p>
       <pre>
         {JSON.stringify(data, null, 2)}
       </pre>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
   )
 }
 
